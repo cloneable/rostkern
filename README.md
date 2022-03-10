483a672ce29ef46bc8ec0cdafa9a7d25b32f2150
Folke Behrens <folke@gmail.com>
2022-03-10 12:39:04 +0100

    Configure toolchain and prep main.rs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rostkern
 
 An attempt at writing a very simply kernel with bootloader in Rust.
+
+## Used Resources
+
+* https://os.phil-opp.com/minimal-rust-kernel/ by Philipp Oppermann.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("rostkern");
+#![no_std]
+#![no_main]
+#![no_implicit_prelude]
+
+extern crate core;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
 }
